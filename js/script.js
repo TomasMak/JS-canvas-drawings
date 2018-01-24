6638dccbc8074ec5be558cca9627d821e92e9c7b
Tomas M <30874339+TomasMak@users.noreply.github.com>
2018-01-24 18:20:06 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -5,47 +5,6 @@ var c = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-
-// rectangles
-
-// c.fillStyle = '#F5F5F5';
-// c.fillRect(0, 50, 291, 100);
-// c.fillRect(289, 148, 30, 50);
-// c.fillStyle = 'black';
-// c.fillRect(321, 200, 100, 100);
-// c.fillStyle = '#FFFAF0';
-// c.fillRect(642, 250, 100, 100);
-// c.fillStyle = 'red';
-// c.fillRect(963, 350, 100, 100);
-// console.log(canvas);
-
-// // line
-
-// c.beginPath();
-// c.moveTo(400, 600);
-// c.lineTo(300, 200);
-// c.lineTo(900, 300);
-// c.strokeStyle = 'white';
-
-// c.stroke();
-
-// Arc / Circle
-// c.beginPath();
-// c.arc(450, 400, 60, 0, Math.PI *2, false);
-// c.strokeStyle = 'black';
-// c.stroke();
-
-// Multiple cirles
-
-// for (var i = 0; i < 3; i++) {
-//   var x = Math.random() * window.innerWidth;
-//   var y = Math.random() * window.innerHeight;
-//   c.beginPath();
-//   c.arc(x, y, 50, 60, 0, Math.PI *2, false);
-//   c.strokeStyle = 'blue';
-//   c.stroke();
-// }
-
 // Animation
 
 var mouse = {
